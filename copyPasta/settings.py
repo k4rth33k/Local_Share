8813516de22310cd07f4f3d043d0e8a1a227e387
KARTHEEK <kartheek2000mike@gmail.com>
2019-11-04 20:54:04 +0530

    Fixed templates dir issue again

--- copyPasta/settings.py
+++ copyPasta/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'copyPasta\\templates')
+            os.path.join(BASE_DIR, 'copyPasta/templates')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
